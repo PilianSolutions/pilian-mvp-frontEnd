783681898112f881b7ffceb86794c15ed2e9ce5c
Mikael Angelo <mikaelangelo12@gmail.com>
2024-03-10 18:30:45 -0300

    inicinando o pilian front

---
+++ src/app/ core/layout/horizontal/horizontal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pilian-horizontal',
+  standalone: true,
+  imports: [],
+  templateUrl: './horizontal.component.html',
+  styleUrl: './horizontal.component.scss'
+})
+export class HorizontalComponent {
+
+}
---
+++ src/app/page/login/component/acessar-conta/acessar-conta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pilian-acessar-conta',
+  standalone: true,
+  imports: [],
+  templateUrl: './acessar-conta.component.html',
+  styleUrl: './acessar-conta.component.scss'
+})
+export class AcessarContaComponent {
+
+}
